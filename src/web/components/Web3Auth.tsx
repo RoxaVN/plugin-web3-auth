a21bdd1c281b2beae0ed1595675ce0b29c0e9a33
woody <woodpecker146@gmail.com>
2023-10-12 16:40:46 +0700

    feat: add Web3Auth

---
+++ src/web/components/Web3Auth.tsx
@@ -0,0 +1,38 @@
+import { Modal } from '@mantine/core';
+import { webModule } from '@roxavn/core/web';
+import { useWeb3Modal } from '@web3modal/wagmi/react';
+import { useState } from 'react';
+
+import { useWeb3Auth } from '../hooks/index.js';
+
+export interface Web3AuthProps {
+  registerComponent: JSX.Element;
+  guestComponent: (openWeb3Modal: () => void) => JSX.Element;
+  userComponent: (data: {
+    user: Record<string, any>;
+    address: string;
+  }) => JSX.Element;
+}
+
+export function Web3Auth({
+  userComponent,
+  guestComponent,
+  registerComponent,
+}: Web3AuthProps) {
+  const [open, setOpen] = useState(false);
+  const { user, address } = useWeb3Auth(() => setOpen(true));
+  const web3Modal = useWeb3Modal();
+  const { t } = webModule.useTranslation();
+
+  if (open) {
+    return (
+      <Modal opened={open} onClose={() => setOpen(false)} title={t('register')}>
+        {registerComponent}
+      </Modal>
+    );
+  }
+  if (user && address) {
+    return userComponent({ user, address });
+  }
+  return guestComponent(web3Modal.open);
+}
